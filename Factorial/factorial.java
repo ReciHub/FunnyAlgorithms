84d1764f414bbaf9f44e2d72364b28cffbba00b4
unknown <xxredstonetekxx@gmail.com>
2018-10-05 10:02:27 -0500

    Added Factorial algorithm for Java and JavaScript

---
+++ Factorial/factorial.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static long factorial(int n){
+        long accumulator = 1;
+
+        for(; n > 0; n--){
+            accumulator *= n;
+        }
+
+        return accumulator;
+    }
+
+    public static boolean isInteger(String text){
+        try{
+            Integer.parseInt(text);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number to factorialize: ");
+        String input = scanner.nextLine();
+
+        if(isInteger(input)){
+            try{
+                int num = Integer.parseInt(input);
+                System.out.println("The factorial of " + num + " is " + factorial(num));
+            }catch(NumberFormatException e){
+                System.out.println("What happened there ?");
+            }
+        }else {
+            System.out.println("The input was not a number");
+        }
+    }
+}
