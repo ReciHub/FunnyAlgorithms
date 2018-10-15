0445eec9d0b8a3f8b6dd2663522bd8cea71d92bf
ramkrishna maheta <rmaheta@mobileiron.com>
2018-10-15 19:34:01 +0530

    spiral-order-traversal java add sample #16

--- Spiral Order of Matrix/Spiral_Order.java
+++ Spiral Order of Matrix/Spiral_Order.java
@@ -2,6 +2,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
+// sample input 
+// 4 5
+// 1 2 3 4 5
+// 6 7 8 9 10
+// 11 12 13 14 15
+// 16 17 18 19 20
+
+
 public class Spiral_Order{
     public static void main(String args[]) throws IOException{
     	BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
