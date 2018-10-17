b1947f315c1bd48d0befa7476d4f28e740ae462f
Vinícius Alencar <viniciusale4@gmail.com>
2018-10-17 16:45:20 -0300

    added comments

--- Factorial/factorial.go
+++ Factorial/factorial.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func factorial(x uint) uint {
-	if x == 0 { return 1 }
-	return x * factorial(x-1)
+	if x == 0 { return 1 }   // if x equals 0, returns 1 as neutral multiplication element (and following factorial definition)
+	return x * factorial(x-1)// else, returns the current x value times its predecessor
 }
 
 func main() {
 	x := uint(7)
 	fact := factorial(x)
-	fmt.Println(fact)
+	fmt.Println(fact) // expects 5040 to be printed
 }
