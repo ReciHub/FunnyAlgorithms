50d91336170c73c421e64a93c13639f9e85ca821
hafpaf <haf@hafnium.me>
2018-10-26 11:22:18 +0200

    set variable "num" to generate a random number between 100 and 9999

--- Armstrong Numbers/ArmstrongNumber.py
+++ Armstrong Numbers/ArmstrongNumber.py
@@ -1,6 +1,8 @@
-num = 1634
+import random
+num = random.randrange(100, 9999) #random number between 100 and 9999
+#try to set the variable "num" to either 407 or 1634
 
-# Changed num variable to string, 
+# Changed num variable to string,
 # and calculated the length (number of digits)
 order = len(str(num))
 
@@ -10,7 +12,7 @@ sum = 0
 # find the sum of the cube of each digit
 temp = num
 while temp > 0:
-   digit = temp % 10
+   digit = temp % 10 #modulo operator
    sum += digit ** order
    temp //= 10
 
