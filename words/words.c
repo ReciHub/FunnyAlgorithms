4f10b49de7c83bece49130b30a65f997fe49430e
Jacob Latonis <jlatonis@me.com>
2019-10-01 09:04:25 -0500

    Update words.c
    Fixed spacing and indentation

--- words/words.c
+++ words/words.c
@@ -5,18 +5,18 @@
 int main() 
 { 
 	char *buffer = malloc(sizeof(char)*100);
-    char *token = malloc(sizeof(char)*100);
+   	char *token = malloc(sizeof(char)*100);
   	if (buffer == NULL || token == NULL) {
-    	printf("Error allocating memory");
-      	exit(1);
-    }
-  	int wordCount = 0;
-      fgets(buffer, 512, stdin);
-      if (buffer != NULL) {
-        while ((token = strtok_r(buffer, " \n\t", &buffer))) {
-          wordCount++;
-        }
-    }
-  printf("Your text contains %d words\n", wordCount);
-  return 0;
+		printf("Error allocating memory");
+      		exit(1);
+    	}
+ 	int wordCount = 0;
+      	fgets(buffer, 512, stdin);
+     	if (buffer != NULL) {
+       		while ((token = strtok_r(buffer, " \n\t", &buffer))) {
+         		wordCount++;
+        	}
+    	}
+	printf("Your text contains %d words\n", wordCount);
+	return 0;
 } 
