4d4f3b9e6b5305f7279656eca73a77a0f6d631e2
jonasvag <jonasvag777@gmail.com>
2019-10-01 20:14:34 +0300

    Implement UnSort algorithm in haskell

---
+++ UnSort/UnSort.hs
@@ -0,0 +1,17 @@
+import System.Random
+import Data.Array.IO
+import Control.Monad
+
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+        ar <- newArray n xs
+        forM [1..n] $ \i -> do
+            j <- randomRIO (i,n)
+            vi <- readArray ar i
+            vj <- readArray ar j
+            writeArray ar j vi
+            return vj
+  where
+    n = length xs
+    newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray n xs =  newListArray (1,n) xs
