52721f903bdb9b851b49680d3fd2c18a3ff6c084
Lucas Soares <lsoares267@gmail.com>
2019-10-01 16:18:30 -0300

    Js Bubble sort

---
+++ bubble_sort/bubble_sort.js
@@ -0,0 +1,18 @@
+let a = [];
+
+function bubbleSort(a) {
+    var swapped;
+    do {
+        swapped = false;
+        for (var i=0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                var temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
+
+bubbleSort(a);
