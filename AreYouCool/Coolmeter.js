2c620ed5c526a624f79b41e034e6441fb4002729
Shashank <Shshankm597@gmail.com>
2019-10-04 00:52:53 +0530

    Added Are you cool algorithm JS version

---
+++ AreYouCool/Coolmeter.js
@@ -0,0 +1,44 @@
+// Code by:Shashank
+// Github: github.com/shshankm597
+Math.floor(Math.random() * 20);
+function Coolmeter () {
+  const  x = Number(prompt("From 0-100 how cool you think you are?: "));
+  let i = 0
+    let lista =[]
+    while(i!= x) {
+        
+        let value = ran.randrange(x+1)
+        (if value in lista)
+            continue
+        (if value == 0)
+            continue
+        lista.append(value)
+        i = i+1
+    }
+    let posi = ran.randrange(x)
+    if(lista[posi]>x/2):
+        print("You are cool!")
+    elif(lista[posi]<x/2):
+        print("You are not cool!")
+    else:
+        print("You are the rarest kind of cool")
+
+}
+// while True:
+    i = 0
+    lista =[]
+    while(i!= x):
+        value = ran.randrange(x+1)
+        if value in lista:
+            continue
+        if value == 0 :
+            continue
+        lista.append(value)
+        i = i+1
+    posi = ran.randrange(x)
+    if(lista[posi]>x/2):
+        print("You are cool!")
+    elif(lista[posi]<x/2):
+        print("You are not cool!")
+    else:
+        print("You are the rarest kind of cool")
