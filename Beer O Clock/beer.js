991c012537c738998524ce7cb89f4e6eab1885bb
Luiz <lf1405@hotmail.com>
2019-10-03 23:43:34 -0300

    Wrinting beer o clock in javascript

---
+++ Beer O Clock/beer.js
@@ -0,0 +1,19 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question(
+  `Should I check if it is time to have Beer ? (Y / N): `,
+  result => {
+    result = result.toString().toLowerCase();
+    if (result == "y") {
+      console.log("It always is beer of clock. Cheers !!!");
+    } else if (result == "n") {
+      console.log("We check later.");
+    } else {
+      console.log("Invalid input, try again.");
+    }
+    readline.close();
+  }
+);
