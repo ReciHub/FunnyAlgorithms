c2d95cc96e781db3da48386d99f153d2b73f3a4b
JinapriyaMadushanka <50899862+JinapriyaMadushanka@users.noreply.github.com>
2019-10-06 06:25:53 +0530

    Create longestWord.py

---
+++ longestWord/longestWord.py
@@ -0,0 +1,7 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Acer\Documents\GitHub\FunnyAlgorithms\longestWord\longestWord.py 
+Enter a string: vb23 dfgdgdfg
+dfgdgdfg
+>>> 
