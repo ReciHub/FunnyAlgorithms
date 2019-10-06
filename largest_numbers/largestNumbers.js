e479a0cba83a14d0b10d570d1d7a4e51cbbfc00e
JinapriyaMadushanka <50899862+JinapriyaMadushanka@users.noreply.github.com>
2019-10-06 06:52:38 +0530

    Create largestNumbers.js

---
+++ largest_numbers/largestNumbers.js
@@ -0,0 +1,17 @@
+function largestOfFour(arr) {
+    var results = [];
+    for (var n = 0; n < arr.length; n++) {
+      var largestNumber = arr[n][0];
+      for (var sb = 1; sb < arr[n].length; sb++) {
+        if (arr[n][sb] > largestNumber) {
+          largestNumber = arr[n][sb];
+        }
+      }
+  
+      results[n] = largestNumber;
+    }
+  
+    console.log(results);
+  }
+  
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
