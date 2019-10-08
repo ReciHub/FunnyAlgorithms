a087bb1567095f9e7df0e4aaa9fa913b5e322d56
Kamuish <33263706+Kamuish@users.noreply.github.com>
2019-10-08 13:45:52 +0100

    Create StringReverser.py

---
+++ reverseString/StringReverser.py
@@ -0,0 +1,6 @@
+def reverser(text):
+  """
+    Uses Python built in reverse function to reverse a string. Afterwards, converts it to string.
+  """
+  return ''.join([i for i in reversed(text)])
+  
