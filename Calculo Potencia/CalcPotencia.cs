f9002c5aad80e8ea2f2d11ef7ed18d8596055326
acferM <matheus.acfer@nave.org.br>
2019-10-09 20:53:47 -0300

    Calculo da potencia realizada por um corpo (física)

---
+++ Calculo Potencia/CalcPotencia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_trabalho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double potencia, trabalho, tempo;
+
+            Console.Write("Digite o trabalho em (J) exercido pelo corpo: ");
+            trabalho = double.Parse(Console.ReadLine());
+            Console.Write("Digite o tempo em (s) que o corpo leva para realizar ação: ");
+            tempo = double.Parse(Console.ReadLine());
+            potencia = trabalho / tempo;
+
+            Console.WriteLine(potencia);
+        }
+    }
+}
