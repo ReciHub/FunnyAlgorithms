0184d8764646431946932d4b90d17b06816599d9
Shashank S Mishra <shshankm597@gmail.com>
2019-10-09 22:30:40 +0530

    updated are you cool algorithm JS code

--- AreYouCool/Coolmeter.js
+++ AreYouCool/Coolmeter.js
@@ -1,44 +1,26 @@
 // Code by:Shashank
 // Github: github.com/shshankm597
 Math.floor(Math.random() * 20);
+
 function Coolmeter () {
-  const  x = Number(prompt("From 0-100 how cool you think you are?: "));
-  let i = 0
-    let lista =[]
+  const x = Number(prompt("From 0-100 how cool you think you are?: "));
+  let i = 0;
+    let lista =[];
     while(i!= x) {
         
-        let value = ran.randrange(x+1)
-        (if value in lista)
-            continue
-        (if value == 0)
-            continue
-        lista.append(value)
-        i = i+1
+        let value = Math.random(x+1);
+        if (lista.includes(value) == true)
+            continue;
+        if (value == 0)
+            continue;
+        lista.push(value);
+        i = i+1;
     }
-    let posi = ran.randrange(x)
-    if(lista[posi]>x/2):
-        print("You are cool!")
-    elif(lista[posi]<x/2):
-        print("You are not cool!")
-    else:
-        print("You are the rarest kind of cool")
-
-}
-// while True:
-    i = 0
-    lista =[]
-    while(i!= x):
-        value = ran.randrange(x+1)
-        if value in lista:
-            continue
-        if value == 0 :
-            continue
-        lista.append(value)
-        i = i+1
-    posi = ran.randrange(x)
-    if(lista[posi]>x/2):
-        print("You are cool!")
-    elif(lista[posi]<x/2):
-        print("You are not cool!")
-    else:
-        print("You are the rarest kind of cool")
+    let posi = Math.random(x);
+    if(lista[posi]>x/2)
+        prompt("You are cool!");
+    else if(lista[posi]< x/2)
+        prompt("You are not cool!");
+    else
+    prompt("You are the rarest kind of cool");
+};
