d7a79d3013f9182ef10c930633bc90ef0de1a569
Vinayakjaiswal07 <vinayakjaiswal48@gmail.com>
2019-10-10 17:19:52 +0530

    adding palindrme checker in python

---
+++ PalindromeChecker/PalindromeChecker.py
@@ -0,0 +1,9 @@
+def PalindromeCheck(s):
+	if(s==s[::-1]):
+		return True
+	return False
+s=input()
+if(PalindromeCheck(s)):
+	print("It is a palindrome!!")
+else:
+	print("It is not a palindrome..")
