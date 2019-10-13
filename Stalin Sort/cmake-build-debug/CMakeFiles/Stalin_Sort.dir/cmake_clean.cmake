c26a70b8542ea5284071c1d9fea691a2ee1d2dce
Rohan Nishant <50524832+rondon1947@users.noreply.github.com>
2019-10-13 15:43:18 +0530

    Added the Program for Stalin Sort in C++.

---
+++ Stalin Sort/cmake-build-debug/CMakeFiles/Stalin_Sort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stalin_Sort.dir/main.cpp.o"
+  "Stalin_Sort.pdb"
+  "Stalin_Sort.exe"
+  "libStalin_Sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stalin_Sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
