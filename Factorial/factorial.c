09bda0511e6663602154f82e1110704cc122955b
shubhmittal <43489788+shubhmittal@users.noreply.github.com>
2019-10-14 22:14:29 +0530

    factorial
    Older program was longer and little bit hard to understand, but this program is short and easy to understand.In this I have only used the for loop which makes the program easier to understand.

--- Factorial/factorial.c
+++ Factorial/factorial.c
@@ -1,26 +1,16 @@
-// C program to find factorial of given number 
+#include <stdio.h>
+#include <conio.h>
 
-#include<stdio.h> 
-
-typedef long long int llu
-
-// function to find factorial of given number 
-
-llu factorial(llu n)       //Recursive function for the factorial
-{ 
-	if (n == 0) {
-		return 1;      //Base case for recursive function factorial
-	}
-	
-	else{
-		return n*factorial(n-1);     //If base case is not achieved
-	}
-} 
-
-int main()                         //Driver Function
-{ 
-	llu num;
-	scanf(%lld, &num)
-	printf("Factorial of %d is %d", num, factorial(num)); 
-	return 0;
+void main()
+{
+    int i, n, product;
+    printf("enter a number \n");
+    scanf("%d",&n);
+    product=1;
+    for(i=n; i>=1; i--)
+    {
+        product=product*i;
+    }
+    printf("%d",product);
+    getch();
 }
