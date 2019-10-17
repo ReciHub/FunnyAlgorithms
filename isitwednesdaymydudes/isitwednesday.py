7f5907f5e6cb16ccb69fc48f9f96fd352ee91b14
adamyeet <56128585+adamyeet@users.noreply.github.com>
2019-10-17 17:37:36 +0100

    Add countdown
    Update to add a countdown until the next Wednesday, my dudes.

--- isitwednesdaymydudes/isitwednesday.py
+++ isitwednesdaymydudes/isitwednesday.py
@@ -1,7 +1,16 @@
 import datetime as day
+
 myDay = day.datetime.today().weekday()
 
+
+
 if (myDay == "2"):
+
     print("It is Wednesday, my dudes\n")
+
 else: 
-    print("It is not Wednesday, my dudes\n")
+    if (myDay < 2):
+        Daysleft = 2 - myDay
+    else:
+        Daysleft = 9 - myDay
+    print("It is",Daysleft,"days until Wednesday, my dudes\n")
