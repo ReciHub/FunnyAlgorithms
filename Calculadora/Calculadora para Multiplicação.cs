51e1c90aee9b1f73b3f5f21179045826cfb2ec31
Guilherme Martin <gsilvamartin@mgail.com>
2019-10-18 17:28:56 -0300

    Finalização da implementação

---
+++ Calculadora/Calculadora para Multiplicação.cs
@@ -0,0 +1,22 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int num1, num2;
+
+     	Console.Write("Digite o primeiro número: ");
+    	num1 = int.Parse(Console.ReadLine());
+    	
+        Console.Write("Digite o segundo número: ");
+    	num2 = int.Parse(Console.ReadLine());
+
+    	Console.WriteLine(Soma(num, num2));
+  	}
+
+  	public static int Multiplica(int num1, int num2)
+  	{
+    	return num1 * num2;
+  	}
+}
