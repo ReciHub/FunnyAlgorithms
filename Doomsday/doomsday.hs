34994349ebe7c136aa8c5189ac90595bb6b593b1
Saverio <savi.under@gmail.com>
2019-10-18 20:57:59 +0200

    Add Doomsday algorithm in Haskell

---
+++ Doomsday/doomsday.hs
@@ -0,0 +1,43 @@
+import Data.List
+import System.IO
+
+main :: IO ()
+main = return()
+
+doomsdayLeap = [4,1,7,4,2,6,4,1,5,3,7,5]
+doomsdayNotLeap = [3,7,7,4,2,6,4,1,5,3,7,5]
+
+printWeekday :: Int -> String
+printWeekday 0 = "Sunday"
+printWeekday 1 = "Monday"
+printWeekday 2 = "Tuesday"
+printWeekday 3 = "Wednesday"
+printWeekday 4 = "Thursday"
+printWeekday 5 = "Friday"
+printWeekday 6 = "Sunday"
+
+getWeekday :: (Int,Int,Int) -> Int
+getWeekday (d,m,y)
+  | (d<1 || d>31) = -1
+  | (m<1 || m>12) = -1
+  | (y<1990 || y>2019) = -1
+  | (m == 2 && d > 29) = -1
+  | otherwise = dday
+  where dday = (d1 + (d - h)) `mod` 7
+        d1 = c4 `mod` 7
+        c4 = c1 + c2 + c3 + k
+        c3 = c2 `div` 4
+        c2 = c0 - (c1 * 12)
+        c1 = c0 `div` 12
+        c0 = y `mod` 100
+        h = getAnchorDay m y
+        k = getYearValue y
+
+getYearValue x
+  | (x<2000) = 3
+  | otherwise = 2
+
+getAnchorDay m y
+  | (y `mod` 100 == 0) && not (y `mod` 400 == 0) = doomsdayNotLeap !! (m-1)
+  | (y `mod` 4 == 0) = doomsdayLeap !! (m-1)
+  | otherwise = doomsdayNotLeap !! (m-1)
