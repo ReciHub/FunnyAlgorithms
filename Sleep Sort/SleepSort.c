9c7389ac0138526f53bba098698dacdf01d35ca0
Heagan <georgesferopoulos@yahoo.com>
2019-10-21 10:32:56 +0200

    Update SleepSort.c

--- Sleep Sort/SleepSort.c
+++ Sleep Sort/SleepSort.c
@@ -3,31 +3,44 @@
 #include <unistd.h>  //Header file for sleep(). man 3 sleep for details. 
 #include <pthread.h> 
 
-#define N_ELEMENTS 10
 
+/*
+The worker function will be called in a tread and given an address to a number, 
+after getting the number from the address it will then sleep for that numbers value and then print out the number
+*/
 void *worker(void *n) 
 {
+    // Fancy pointer magic
     int num = (int)*(int *)n;
+    // Main function!
     sleep(num);
-    printf("%d", num); 
+    // prints the number after sleep
+    printf("%d", num);
     return NULL; 
 } 
 
 
 int main(int argc, char **argv) 
 {
-    int numbers[N_ELEMENTS] = {4, 3, 6, 1, 5, 7, 0, 9, 2, 8};
+    // Initilise a quick array
+    int numbers[10] = {4, 3, 6, 1, 5, 7, 0, 9, 2, 8};
+    // The varible that makes threading possible
     pthread_t t;
 
     int i = 0;
-    while (i < N_ELEMENTS)
+    while (i < 10)
     {
+        // Please create a thread for each number
         pthread_create(&t, NULL, worker, (void *)&numbers[i]);
         i++;
     }
     
+    // Join all your created threads to this process
+    // Then when your computer runs this process and its threads it will also run the threads we created!
     pthread_join(t, NULL);
+    // Wait for printf to do its job, its sometimes slow to print stuff
     sleep(1);
+    // Put a newline
     puts("");
 }
 
