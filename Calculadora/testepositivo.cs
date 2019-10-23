637b301f9cdb45bc218bf56ddf69378fa7371525
ArturCosta16 <arturcostacd@gmail.com>
2019-10-23 11:39:26 -0300

    teste de positivo ou negativo

---
+++ Calculadora/testepositivo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static bool TestePositivo(int a)
+        {
+            if (a >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número para verificar se é positivo ou não");
+            int a = int.Parse(Console.ReadLine());
+            TestePositivo(a);
+            if (TestePositivo(a))
+            {
+                Console.WriteLine("É positivo");
+            }
+            else
+            {
+                Console.WriteLine("Não é positivo");
+            }
+
+        }
+    }
+}
