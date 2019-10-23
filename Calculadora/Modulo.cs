358a0fb2d3bc3c87a67af61adfca782c80250a90
ArturCosta16 <arturcostacd@gmail.com>
2019-10-23 11:35:42 -0300

    calculadora de módulo

---
+++ Calculadora/Modulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static int Md(int b)
+        {
+            if (b >= 0)
+            {
+                return b;
+            }
+            else
+            {
+                return (b * (-1));
+            }
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número para verificar seu módulo");
+            int b = int.Parse(Console.ReadLine());
+            Md(b);
+            Console.WriteLine(Md(b));
+
+        }
+    }
+}
