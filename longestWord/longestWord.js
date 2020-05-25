8ad2383eb006c3d09f4951e73ba00987d93f0751
Louay-Rouabeh <louayrouabeh46@gmail.com>
2020-05-25 15:36:11 +0100

    longestWord in java and javascript

---
+++ longestWord/longestWord.js
@@ -0,0 +1,12 @@
+function longestWord(string) {
+    var str = string.split(" ");
+    var longest = 0;
+    var word = null;
+    for (var i = 0; i < str.length - 1; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
+}
