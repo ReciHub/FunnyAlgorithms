83c68439c3401ea60c3f220204004448ae0e466d
Ak_0330 <ak03032000@gmail.com>
2020-07-30 13:30:49 +0530

    Count number of characters in the word if text is one word

--- words/words.py
+++ words/words.py
@@ -1,4 +1,7 @@
 s = input("enter your text: ")
-n = len(s.split())
-
-print("your text contains " + str(n) + " words")
+if len(s.split()) > 1:       # If the text is multi word, it will count the number of words
+    n = len(s.split())
+    print("your text contains " + str(n) + " words")
+elif len(s.split()) == 1:    # If the text is one word, it will count the number of characters
+    a = len(s.strip())
+    print("your text contains " + str(a) + " characters")
