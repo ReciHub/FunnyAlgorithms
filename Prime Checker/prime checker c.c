25dd9f7621c836cff61c585747175c2503a1067a
RASHI SINGH <rashisingh@ieee.org>
2020-09-24 22:51:54 +0530

    Update prime checker c.c

--- Prime Checker/prime checker c.c
+++ Prime Checker/prime checker c.c
@@ -5,17 +5,17 @@ int prime(int n)
 	int i;
   	if (n == 1) 
 	{
-    return 0;
+    		return 0;
   	}
   	else 
 	{
-    	for (i = 2; i < n; i++) 
+    		for (i = 2; i < n; i++) 
 		{
-      		if (n % i == 0)
+      			if (n % i == 0)
 			{
-        		return 0;
-      		}
-    	}
+        			return 0;
+      			}
+    		}
     	return 1;
   	}
 }
