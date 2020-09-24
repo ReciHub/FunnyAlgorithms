5829a17ee7f401f2c1c2498bb4d47c1a8ad1b71f
RASHI SINGH <rashisingh@ieee.org>
2020-09-24 22:57:04 +0530

    Update prime checker.cpp

--- Prime Checker/prime checker.cpp
+++ Prime Checker/prime checker.cpp
@@ -12,9 +12,9 @@ int prime(int n)
     		for (int i = 2; i < n; i++) 
 		{
       			if (n % i == 0)
-				{
-        				return 0;
-      				}
+			{
+        			return 0;
+      			}
     		}
     		return 1;
   	}
