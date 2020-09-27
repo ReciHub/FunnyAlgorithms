309b97c5f9acb632049ff86b903fe0b40d17f432
RASHI SINGH <rashisingh@ieee.org>
2020-09-27 10:55:22 +0530

    Update prime checker c.c

--- Prime Checker/prime checker c.c
+++ Prime Checker/prime checker c.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 
-int prime(int n)
+int prime(int n) 
 {
-	int i;
-  	if (n == 1) 
-	{
-    		return 0;
-  	}
-  	else 
-	{
-    		for (i = 2; i < n; i++) 
-		{
-      			if (n % i == 0)
-			{
-        			return 0;
-      			}
-    		}
-    	return 1;
-  	}
+  int i;
+  if (n == 1) 
+  {
+    return 0;
+  } 
+  else 
+  {
+    for (i = 2; i < n; i++) 
+    {
+      if (n % i == 0) 
+      {
+        return 0;
+      }
+    }
+    return 1;
+  }
 }
 
 int main() 
 {
-	int n;
-	printf("\n Enter the number to check for prime or not");
-	scanf("%d", &n);
-	int r;
-	r = prime(n);
-	if(r==1)
-	{
-		printf("\n prime");
-	}
-	else
-	{
-		printf("\n not prime");
-	}
-	return 0;
+  int n;
+  printf("\n Enter the number to check for prime or not");
+  scanf("%d", & n);
+  int r;
+  r = prime(n);
+  if (r == 1) 
+  {
+    printf("\n prime");
+  } 
+  else 
+  {
+    printf("\n not prime");
+  }
+  return 0;
 }
