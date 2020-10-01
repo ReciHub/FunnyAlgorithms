803d704db0f5bff3e46ef48f9d13205472cf6e6b
Shivam Raj Singh <48837530+shivam3009@users.noreply.github.com>
2020-10-01 14:29:54 +0530

    Update Anagram.c

--- AnagramChecker/Anagram.c
+++ AnagramChecker/Anagram.c
@@ -4,17 +4,16 @@ int check_for_anagram(char *a , char *b){
 	int i,j;
 	
 	// Creating hash table for alphabets
-    int hash[26]={0};
+    	int hash[26]={0};
 
 	// store the count of each alphabet in string a in hash table
-    for(i=0 ; a[i]!='\0';i++){
+	for(i=0 ; a[i]!='\0';i++){
 		j = a[i]-97;
 		hash[j]= hash[j]+1;
 	}
 
 	// match the hash table for string b and string a
 	for(i=0; b[i]!='\0';i++){
-		
 		j = b[i]-97;
 		hash[j]= hash[j]-1;
 		
@@ -30,7 +29,7 @@ int check_for_anagram(char *a , char *b){
 }
 int main(){
 	char *a = "medical";
-    char *b = "decimal";
+	char *b = "decimal";
 	check_for_anagram(a,b);
 	return 0;
 }
