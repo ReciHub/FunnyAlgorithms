c314648532f060ec8772c3c0407c04db9bf97853
Seggy116 <zingyzak@gmail.com>
2020-10-11 09:23:24 +0100

    Added Comments

--- Text-To-Audio-and-Back/FromWav.py
+++ Text-To-Audio-and-Back/FromWav.py
@@ -1,81 +1,105 @@
 import time
 import wave, struct, math, random
 
-def Morse_to_Txt():
-    txt = morse
-    Out = [k for i in txt.split() for k,v in MORSE_CODE_DICT.items() if i==v]
+#Function For converting back
+def Code_to_Txt():
+    txt = code
+    #Coverts it using the dictionary
+    Out = [k for i in txt.split() for k,v in ENCODE_DICT.items() if i==v]
     newtxt = ''.join(Out)
+    #Removes the two hashtags
     newtxt = newtxt[1:len(newtxt) -1]
+    #Prints
     print(newtxt)
-	
-MORSE_CODE_DICT = { ' ':'//', 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....',
+
+
+#Disctionary containing what to decode it from
+ENCODE_DICT = { ' ':'//', 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....',
                     'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.','O':'---', 'P':'.--.', 'Q':'--.-',
                     'R':'.-.', 'S':'...', 'T':'-','U':'..-', 'V':'...-', 'W':'.--','X':'-..-.', 'Y':'-.--', 'Z':'--..',
                     '1':'.----', '2':'..---', '3':'...--','4':'....-', '5':'.....', '6':'-....','7':'--...', '8':'---..', '9':'----.',
                     '0':'-----', ', ':'--..--', '.':'.-.-.-','?':'..--..', '/':'-..-.', '-':'-....-','[':'-.--.', ']':'-.--.-', '#':'-..-', ':':'---..-.'}
 
+#Opens A New wav For A Slowed Down Output
 WavWrite = wave.open('./Output.wav', 'w')
+#Opens The Wav File For Reading
 WavRead = wave.open('./sound.wav', 'r')
+#Opens Wavs For Data Reference
 Dot = wave.open('./Audio/dot.wav', 'r')
 Dash = wave.open('./Audio/dash.wav', 'r')
 Gap = wave.open('./Audio/gap.wav', 'r')
 
+#Gets Those Wav Files Data
 DotFind = Dot.readframes(Dot.getnframes())
 DashFind = Dash.readframes(Dash.getnframes())
 GapFind = Gap.readframes(Gap.getnframes())
 
-
+#Configures The Output
 WavWrite.setnchannels(WavRead.getnchannels())
 WavWrite.setsampwidth(WavRead.getsampwidth())
 WavWrite.setframerate(WavRead.getframerate()/128)
 
+#Gets The Data From Source Wav
 frames = WavRead.readframes(WavRead.getnframes())
+#Pushes it to the output
 WavWrite.writeframes(frames)
 
-morse = ''
+#Sets Variables To Default
+code = ''
 xStart = 10000
 x = xStart
 y = 0
 GapCount = 0
 
+#Loops through data
 while x > 0:
-    #print(len(frames))
     if x == xStart - 4:
-        morse = morse + " "
+        #Fixes A Error with spacing
+        code = code + " "
         GapCount = GapCount + 1
+    #Looks For Data Being Equal To Dotfind and makes sure it is not to long
     if (frames.find(DotFind) != -1) and (frames.find(DotFind) < int(len(DashFind))):
-        #print("Found")
-        #print(str(frames.find(DotFind)) + ' : Dot')
+        #Removes Used Data From Frames
         frames = frames[:frames.find(DotFind)] + frames[frames.find(DotFind) + len(DotFind) - 1:]
-        morse = morse + '.'
+        #Adds the "Morse Code" To the output string
+        code = code + '.'
+        #Resets Gap count
         GapCount = 0
+    #Looks For Data Being Equal To Dashfind and makes sure it is not to long
     elif (frames.find(DashFind) != -1) and (frames.find(DashFind) < int(len(DashFind))):
-        #print("Found")
-        #print(str(frames.find(DashFind)) + ' : Dash')
+        #Removes Used Data From Frames
         frames = frames[:frames.find(DashFind)] + frames[frames.find(DashFind) + len(DashFind) - 1:]
-        morse = morse + '-'
+        #Adds the "Morse Code" To the output string
+        code = code + '-'
+        #Resets Gap count
         GapCount = 0
+    #Looks For Data Being Equal To Gapfind and makes sure it is not to long
     elif (frames.find(DotFind) != -1) and (frames.find(GapFind) < int(len(DashFind))):
-        #print("Found")
-        #print(str(frames.find(GapFind)) + ' : Gap')
+        #Removes Used Data From Frames
         frames = frames[:frames.find(GapFind)] + frames[frames.find(GapFind) + len(GapFind) - 1:]
-        morse = morse + ' '
+        #Adds the "Morse Code" To the output string
+        code = code + ' '
+        #Adds 1 to Gap Count
         GapCount = GapCount + 1
         if GapCount == 4:
-            morse = morse + "// "
+            #Adds a space where it should be
+            code = code + "// "
+    #Ends when done
     else:
         y = y + 1
         if y < 3:
             break
     x-=1
-    #print(x)
 
-print(morse)
+#Prints The Encoded Text
+print(code)
 
+#Closes all open wavs
 WavWrite.close()
 WavRead.close()
 Dot.close()
 Dash.close()
 Gap.close()
 
-Morse_to_Txt()
+#Converts The Encoded Text To Original
+Code_to_Txt()
--- Text-To-Audio-and-Back/ToWav.py
+++ Text-To-Audio-and-Back/ToWav.py
@@ -1,58 +1,87 @@
 import time
 import wave, struct, math, random
 
+#Opens Dot.wav as all audio settings EG
 WavSetter = wave.open('./Audio/dot.wav')
 SampleRate = WavSetter.getframerate()
 SampleWidth = WavSetter.getsampwidth()
 SampleChannels = WavSetter.getnchannels()
 
+#Opens A New Wav File For Output
 obj = wave.open('sound.wav','w')
+#Sets its settings to the EG
 obj.setnchannels(SampleChannels)
 obj.setsampwidth(SampleWidth)
 obj.setframerate(SampleRate*128)
 
+#Closes the EG
 WavSetter.close()
 
-MORSE_CODE_DICT = { ' ':'/', 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....',
+#Disctionary containing what to encode it to
+ENCODE_DICT = { ' ':'/', 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....',
                     'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.','O':'---', 'P':'.--.', 'Q':'--.-',
                     'R':'.-.', 'S':'...', 'T':'-','U':'..-', 'V':'...-', 'W':'.--','X':'-..-', 'Y':'-.--', 'Z':'--..',
                     '1':'.----', '2':'..---', '3':'...--','4':'....-', '5':'.....', '6':'-....','7':'--...', '8':'---..', '9':'----.',
                     '0':'-----', ', ':'--..--', '.':'.-.-.-','?':'..--..', '/':'-..-.', '-':'-....-','[':'-.--.', ']':'-.--.-', '#':'-..-', ':':'---..-.'}
 
-
+#The To Audio Algorythm
 def Txt_to_Morse():
+    #Gets The Text
     txt = input('Enter Text to Convert to Morse: ')
+    #This is a simple fix for an error in the future
     txt = '#' + txt + '#'
-    code = [MORSE_CODE_DICT[i.upper()] + ' ' for i in txt if i.upper() in MORSE_CODE_DICT.keys()]
-    morse=''.join(code)
-    print(morse)
-    for m in morse:
+    #Encodes The Text
+    code = [ENCODE_DICT[i.upper()] + ' ' for i in txt if i.upper() in ENCODE_DICT.keys()]
+    Encoded=''.join(code)
+    #Prints it
+    print(Encoded)
+    #Loops through and converts each symbol to audio
+    for m in Encoded:
         if m=='.':
+            #Opens Wav File As Data
             WavSetter = wave.open('./Audio/dot.wav', 'r')
+            #Gets its Data
             frames = WavSetter.readframes(WavSetter.getnframes())
+            #Writes Data To Output
             obj.writeframes(frames)
+            #Closes Wav File
             WavSetter.close()
         elif m=='-':
+            #Opens Wav File As Data
             WavSetter = wave.open('./Audio/dash.wav', 'r')
+            #Gets its Data
             frames = WavSetter.readframes(WavSetter.getnframes())
+             #Writes Data To Output
             obj.writeframes(frames)
+            #Closes Wav File
             WavSetter.close()
         elif m=='/':
+            #Opens Wav File As Data
             WavSetter = wave.open('./Audio/gap.wav', 'r')
+            #Gets its Data
             frames = WavSetter.readframes(WavSetter.getnframes())
+            #Writes Data To Output
             obj.writeframes(frames)
+            #Closes Wav File
             WavSetter.close()
+
+            #Repetes Above
             WavSetter = wave.open('./Audio/gap.wav', 'r')
             frames = WavSetter.readframes(WavSetter.getnframes())
             obj.writeframes(frames)
             WavSetter.close()
         else:
+            #Opens Wav File As Data
             WavSetter = wave.open('./Audio/gap.wav', 'r')
+            #Gets its Data
             frames = WavSetter.readframes(WavSetter.getnframes())
+             #Writes Data To Output
             obj.writeframes(frames)
+            #Closes Wav File
             WavSetter.close()
 
-
+#Calls the main function
 Txt_to_Morse()
 
+#Ends By Closing The Output Wav
 obj.close()
