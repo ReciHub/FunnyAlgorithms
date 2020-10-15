2808d25ebc5983417cd971cca8c56fb4ff4cb982
Zaheudev <68231115+Zaheudev@users.noreply.github.com>
2020-10-15 12:04:34 +0300

    Binary Search Tree with custom LinkedList

---
+++ Search Binary Tree in java/Node.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Node extends ListItem{
+
+    public Node(Object value) {
+        super(value);
+    }
+
+    @Override
+    ListItem next() {
+        return this.rightLink;
+    }
+
+    @Override
+    ListItem setNext(ListItem item) {
+        this.rightLink = item;
+        return this.rightLink;
+    }
+
+    @Override
+    ListItem previous() {
+        return this.leftLink;
+    }
+
+    @Override
+    ListItem setPrevious(ListItem item) {
+        this.leftLink = item;
+        return this.leftLink;
+    }
+
+    @Override
+    int compareTo(ListItem item) {
+        if(item != null){
+            return ((String) super.getValue()).compareTo((String) item.getValue());
+        }else {
+            return -1;
+        }
+    }
+}
