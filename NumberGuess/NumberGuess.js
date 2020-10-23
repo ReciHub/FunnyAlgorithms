80d9f2c01f656ce10fd824cc53c3a4ad058a90cc
Jonathan Santos <37001764+jsantos93@users.noreply.github.com>
2020-10-23 19:43:40 -0300

    Update NumberGuess.js

--- NumberGuess/NumberGuess.js
+++ NumberGuess/NumberGuess.js
@@ -1,21 +1,5 @@
-//
-// def guess_num():
-//   num=random.randint(1,100)
-//   chance=1
-//   while chance<=5:
-//     n=int(input("Guess The Number : "))
-//     if num==n:
-//       return "You Win"
-//      else:
-//       chance+=1
-//       return "Try Again"
-//     return "You Lose"
- 
-// game_play='Y'
-// while game_play='y' or game_play='Y':
-//   guess_num()
-//   game_play=input("Do You Wanna PLay Again? ")
 var readline = require('readline');
+
 function guess_num(){
     var question = readline.createInterface({
         input: process.stdin,
@@ -23,9 +7,6 @@ function guess_num(){
     });
     
     const num = Math.floor(Math.random() * 100);     // returns a random integer from 0 to 99
-    chance = 1;
-
-
     question.question("Guess The Number :", (answer)=>{
         if (num == answer){
             console.log("You Win!");
@@ -35,10 +16,7 @@ function guess_num(){
             console.log("You Lose");
         }
         question.close()
-    })
-        
-
-    
+    })   
 }
 
 guess_num()
