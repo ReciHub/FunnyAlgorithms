321fb2ec0314251a6ca3ff1b468bfef598081b20
MisterMarv <61189611+MisterMarv@users.noreply.github.com>
2020-10-25 22:19:31 -0300

    A C # calculator and some updates on my others submitts

--- ageGuesser/ageGuesser.cs
+++ ageGuesser/ageGuesser.cs
@@ -1,14 +1,22 @@
-//Corpo - Body
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		//Corpo - Body
 
-Console.WriteLine("Hello welcome! I'm the Birthday Guesser! ");
-Console.WriteLine("Now I'm going to ask you a few questions to know your birthday!");
+    Console.WriteLine("Hello welcome! I'm the Birthday Guesser! ");
+    Console.WriteLine("Now I'm going to ask you a few questions to know your birthday!");
 
-Console.WriteLine("What year were you born?");
-int yearBirth = int.Parse(Console.ReadLine());
+    Console.WriteLine("What year were you born?");
+    int yearBirth = int.Parse(Console.ReadLine());
 
-Console.WriteLine("What year is it now?");
-int yearActual = int.Parse(Console.ReadLine());
+    Console.WriteLine("What year is it now?");
+    int yearActual = int.Parse(Console.ReadLine());
 
-int age = yearActual - yearBirth;
+    int age = yearActual - yearBirth;
 
-Console.WriteLine("Your age is 1,110 years old! Just kidding, your real age is {0}", age);
+    Console.WriteLine("Your age is 1,110 years old! Just kidding, your real age is {0}", age);
+	}
+}
