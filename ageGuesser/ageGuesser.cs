566fbd0b35cc26271d1e7cccca3a453ff846a192
Erick Simões <erick.simoes@live.com>
2020-11-03 10:56:06 -0300

    Update ageGuesser.cs with main class

--- ageGuesser/ageGuesser.cs
+++ ageGuesser/ageGuesser.cs
@@ -1,14 +1,20 @@
-//Corpo - Body
+using System;
 
-Console.WriteLine("Hello welcome! I'm the Birthday Guesser! ");
-Console.WriteLine("Now I'm going to ask you a few questions to know your birthday!");
+class MainClass {
+  public static void Main (string[] args) {
+    //Corpo - Body
 
-Console.WriteLine("What year were you born?");
-int yearBirth = int.Parse(Console.ReadLine());
+    Console.WriteLine("Hello welcome! I'm the Birthday Guesser! ");
+    Console.WriteLine("Now I'm going to ask you a few questions to know your birthday!");
 
-Console.WriteLine("What year is it now?");
-int yearActual = int.Parse(Console.ReadLine());
+    Console.WriteLine("What year were you born?");
+    int yearBirth = int.Parse(Console.ReadLine());
 
-int age = yearActual - yearBirth;
+    Console.WriteLine("What year is it now?");
+    int yearActual = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Your age is 1,110 years old! Just kidding, your real age is {0}", age);
+    int age = yearActual - yearBirth;
+
+    Console.WriteLine("Your age is 1,110 years old! Just kidding, your real age is {0}", age);
+  }
+}
