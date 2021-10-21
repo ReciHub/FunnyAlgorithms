8f1df4e314d6a79ff3fea94afd0686dcae6741e1
Dark <soumyadwip@outlook.in>
2021-10-21 23:25:23 +0530

    Added bitwise operator in C++ language

---
+++ bitwise_operator/bitwise_operator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n=60;
+    int n1=n>>1; // Divide n by 2 by right shifting the bits
+    int n2=n<<1; // Multiply n with 2 by left shifting the bits
+}
