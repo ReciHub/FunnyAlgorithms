21e742a114b3628016ac72c1d0fd906c1bb6efbe
Kei-Seo <ghdtjr3824@naver.com>
2021-11-05 15:35:03 +0900

    impliment kruskal algorithm python

---
+++ Kruskal Algorithm/kruskal.py
@@ -0,0 +1,50 @@
+# input
+# 7 9
+# 1 2 29
+# 1 5 75
+# 2 3 35
+# 2 6 34
+# 3 4 7
+# 4 6 23
+# 4 7 13
+# 5 6 53
+# 6 7 25
+
+# output
+# 159
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)  
+
+edges = []
+result = 0
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    if find(a) != find(b):
+        union(a, b)
+        result += cost
+
+print(result)
