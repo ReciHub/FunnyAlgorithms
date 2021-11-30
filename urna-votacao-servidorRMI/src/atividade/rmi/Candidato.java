366d8b3eb884313011383c1db9da83f59303a817
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-11-30 17:04:07 -0300

    Adicionado arquivos e readme

---
+++ urna-votacao-servidorRMI/src/atividade/rmi/Candidato.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade.rmi;
+
+/**
+ *
+ * @author nelsontoneze
+ */
+public class Candidato {
+
+    private String nome;
+    private int votos;
+    
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the votos
+     */
+    public int getVotos() {
+        return votos;
+    }
+
+    /**
+     * @param votos the votos to set
+     */
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+    
+    
+}
