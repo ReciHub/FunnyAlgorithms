e126396a89a1d87175a1813b12b2d73ad1174272
shibakar <shibakar@swazei.com>
2022-02-20 19:59:47 +0530

    updated code

--- Armstrong Numbers/ArmstrongNumber.dart
+++ Armstrong Numbers/ArmstrongNumber.dart
@@ -2,27 +2,22 @@ import 'dart:math';
 import 'dart:io';
 
 void main() {
- 
   var num = int.parse(stdin.readLineSync().toString());
   var n = num;
-  
+
   var digit;
   var arm = 0;
-  
-  while(num!=0){
-    
-    digit = num%10;
-    
-    arm = arm + pow(digit,3);
-    
-    num=(num/10).toInt();
-    
+
+  while (num != 0) {
+    digit = num % 10;
+
+    arm = (arm + pow(digit, 3)).toInt();
+
+    num = num ~/ 10;
   }
-  
-  if(arm==n)
+
+  if (arm == n)
     print('Armstrong');
   else
     print('Not Armstrong');
-  
-  
 }
