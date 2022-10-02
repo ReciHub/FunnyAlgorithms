87464308f6ce7a04c2576f14979899088b7d8513
Nikhil Gautam <89832530+nikgautamgithub@users.noreply.github.com>
2022-10-02 20:19:12 +0530

    Operations on Matrix

---
+++ Matrix/operation_on_matrix.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void add(int m[3][3], int n[3][3], int sum[3][3])
+{
+  for(int i=0;i<3;i++)
+    for(int j=0;j<3;j++)
+      sum[i][j] = m[i][j] + n[i][j];
+}
+
+void subtract(int m[3][3], int n[3][3], int result[3][3])
+{
+  for(int i=0;i<3;i++)
+    for(int j=0;j<3;j++)
+      result[i][j] = m[i][j] - n[i][j];
+}
+
+void transpose(int matrix[3][3], int trans[3][3])
+{
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      trans[i][j] = matrix[j][i];
+}
+
+void display(int matrix[3][3])
+{
+  for(int i=0; i<3; i++)
+  {
+    for(int j=0; j<3; j++)
+      printf("%d\t",matrix[i][j]);
+
+    printf("\n"); 
+  }
+}
+
+int main()
+{
+
+  int a[3][3];
+  int b[3][3];
+  int c[3][3];
+  int i, j;
+   for(i=0; i<3; i++) {
+      for(j=0;j<3;j++) {
+         printf("Enter value for disp[%d][%d]:", i, j);
+         scanf("%d", &a[i][j]);
+      }
+   }
+   for(i=0; i<3; i++) {
+      for(j=0;j<3;j++) {
+         printf("Enter value for disp[%d][%d]:", i, j);
+         scanf("%d", &b[i][j]);
+      }
+   }
+ 
+  printf("First Matrix:\n");
+  display(a);
+  printf("Second Matrix:\n");
+  display(b);
+  int choice;
+  do
+  {
+ 
+    printf("\nChoose the matrix operation,\n");
+    printf("----------------------------\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Transpose\n");
+    printf("4. Exit\n");
+    printf("----------------------------\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+      case 1:
+        add(a, b, c);
+        printf("Sum of matrix: \n");
+        display(c);
+        break;
+      case 2:
+        subtract(a, b, c);
+        printf("Subtraction of matrix: \n");
+        display(c);
+        break;
+      case 3:
+        printf("Transpose of the first matrix: \n");
+        transpose(a, c);
+        display(c);
+        printf("Transpose of the second matrix: \n");
+        transpose(b, c);
+        display(c);
+        break;
+      case 4:
+        printf("Thank You.\n");
+        exit(0);
+      default:
+        printf("Invalid input.\n");
+        printf("Please enter the correct input.\n");
+    }
+  }while(1);
+
+  return 0;
+}
