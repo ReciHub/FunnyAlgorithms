fd90a6b6307e9aa070ae614bcfb3747cdd51979c
Yuvi Malhotra <89199719+yuvimalhotra28@users.noreply.github.com>
2022-10-03 00:35:36 +0530

    Updaa

--- Blastoff/blastoff.cpp
+++ Blastoff/blastoff.cpp
@@ -5,9 +5,9 @@ int main()
 {
     int n;
     cout << "How much you wanna blast: ";   //gettig input from the user
-    cin >> n;
+    cin >> n;      
     
-    cout << endl << endl;
+    cout << endl << endl;    // endl to print in new line
     
     cout << "Before Blasting: ";  
     for(int i=1;i<=n;i++) cout << i <<' ';   //before reversing
@@ -15,7 +15,9 @@ int main()
     cout << endl << endl;
     
     cout << "After Blasting: ";
-    for(int i=1;i<=n;i++) cout << n-i+1 <<' ';   //after reversing
+    for(int i=1;i<=n;i++) cout << n-i+1 <<' ';   //Accessing elements from the last of the loop
+    
+    
 
     return 0;
 }
