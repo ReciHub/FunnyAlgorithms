22af3f2abd4e2c580b46a53a9e66d3de77b088f1
Shivam Raj Singh <shivamrajsingh3009@gmail.com>
2022-10-06 02:08:04 +0530

    Added the comments

--- sort/shell/ShellSort.java
+++ sort/shell/ShellSort.java
@@ -8,19 +8,33 @@ public class ShellSort
             System.out.print(arr[i] + " ");
         System.out.println();
     }
-  
+    
+     /* function to sort arr using shellSort */
     public int sort(int arr[])
     {
+         // Start with a big gap(say k), then reduce the k
+
         int n = arr.length;
         for (int k = n/2; k > 0; k /= 2)
         {
+            // Do a gapped insertion sort for this k size.
+            // The first k elements a[0..k-1] are already
+            // in gapped order keep adding one more element
+            // until the entire array is k sorted
             for (int i = k; i < n; i += 1)
             {
+                // add arr[i] to the elements that have been k
+                // sorted save arr[i] in temp and make a hole at
+                // position i
                 int temp = arr[i];
+                // shift earlier k-sorted elements up until
+                // the correct location for arr[i] is found
                 int j;
                 for (j = i; j >= k && arr[j - k] > temp; j -= k)
                     arr[j] = arr[j - k];
-
+                
+                // put temp (the original arr[i]) in its correct
+                // location
                 arr[j] = temp;
             }
         }
