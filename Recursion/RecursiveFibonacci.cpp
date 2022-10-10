dbe6e65e487dc8c6d703b56e18698d18e6706e7d
KORLA GOUTHAM <96729391+KORLA2@users.noreply.github.com>
2022-10-10 17:45:37 +0530

    Create RecursiveFibonacci.cpp

---
+++ Recursion/RecursiveFibonacci.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int fibonacci(int n){
+  
+    if(n==1)
+      return 0;
+    
+    if(n==2)return 1;
+    
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+
+int main(){
+   
+ int n, n1 = 0, n2 = 1, n3 = 0;
+
+    cout << "Enter number: ";
+   
+    cin >> n;
+
+    cout << "Fibonacci sequence: "<<n1<<" "<<n2;
+
+    for(int i=3;i<=n;++i)
+
+    cout<<" "<<fibonacci(i);
+    
+    return 0;
+}
