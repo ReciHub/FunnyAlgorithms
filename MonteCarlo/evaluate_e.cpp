2b635def8461d781d8a2635de98685d56cd27302
Pankaj Jangid <54581594+JangidPK@users.noreply.github.com>
2022-10-10 15:08:09 +0530

    Monte Carlo code to evaluate value of 'e'

---
+++ MonteCarlo/evaluate_e.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+    int Titer = 1000000; // Total iterations for simulation
+    double sum,full_sum;
+    int num;
+    for (int iter =0; iter< Titer; iter++)
+    {
+        srand(iter + time(NULL));
+        sum = 0;
+        num = 0;
+        while(sum <1)
+        {
+            sum += ((double) rand() / (RAND_MAX));
+            num++;
+        }
+        full_sum += num;
+    }
+    cout<< "Value of e is : "<<full_sum/Titer<<endl;
+    
+}
