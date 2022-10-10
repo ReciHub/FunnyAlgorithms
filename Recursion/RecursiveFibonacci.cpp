cafd0ccc7b28a72853e14cbd5cdf242b0aae55c0
KORLA GOUTHAM <96729391+KORLA2@users.noreply.github.com>
2022-10-10 19:34:58 +0530

    Update RecursiveFibonacci.cpp

--- Recursion/RecursiveFibonacci.cpp
+++ Recursion/RecursiveFibonacci.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 
-int fibonacci(int n){
+int fibonacci( int n){
   
-    if(n==1)
+    if(n == 1)
       return 0;
     
-    if(n==2)return 1;
+    if(n == 2) return 1;
     
-    return fibonacci(n-1)+fibonacci(n-2);
+    return fibonacci(n - 1) + fibonacci( n - 2 );
 }
 
 int main(){
@@ -21,12 +21,12 @@ int main(){
     cout << "Enter number: ";
    
     cin >> n;
+ 
+    cout << "Fibonacci sequence: " << n1 << " " << n2;
 
-    cout << "Fibonacci sequence: "<<n1<<" "<<n2;
+    for( int i = 3;i <= n; ++i )
 
-    for(int i=3;i<=n;++i)
-
-    cout<<" "<<fibonacci(i);
+    cout << " " << fibonacci( i );
     
     return 0;
 }
