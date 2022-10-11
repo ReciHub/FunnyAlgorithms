2e835fd8a0d4cfc69074d8a453eb3a66a89df3ab
sachin2496 <sachin.kumar.cse20@itbhu.ac.in>
2022-10-12 00:10:07 +0530

    added html and js file

---
+++ Calculator in js and html/calc.js
@@ -0,0 +1,15 @@
+function dis(val)
+        {
+        document.getElementById("sac").value+=val;}
+        //function for evaluation
+        function solve()
+        {
+        let x = document.getElementById("sac").value;
+        let y = eval(x);
+        document.getElementById("sac").value = y;
+        }
+        //function for clearing the display
+        function clr()
+        {
+        document.getElementById("sac").value = "";
+        }
