fec64040c90adfa943375cea1e3047674fa71584
João Victor Bravo <bravo@MacBook-Pro-de-Joao.local>
2022-10-10 22:28:34 -0300

    Adding Kth Element Python Code

---
+++ Kth Element/smallest-Kth-element.py
@@ -0,0 +1,20 @@
+# Time Complexity of this solution is O(N Log N)
+
+# Function to return k'th smallest  
+# element in a given array  
+def kthSmallest(arr, n, k): 
+  
+    # Sort the given array  
+    arr.sort() 
+  
+    # Return k'th element in the  
+    # sorted array  
+    return arr[k-1] 
+  
+# Driver code 
+if __name__=='__main__': 
+    arr = [12, 3, 5, 7, 19] 
+    n = len(arr) 
+    k = 2
+    print("K'th smallest element is", 
+          kthSmallest(arr, n, k)) 
