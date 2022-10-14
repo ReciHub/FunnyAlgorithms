d5a41ff87eb7a2ccf4929a28745f263fe9db98b3
Sai_Priya <75333911+Saipriyavadisina@users.noreply.github.com>
2022-10-14 19:52:38 +0530

    Changes are made

--- MatchingSubsequence/matchingSubsequences.cpp
+++ MatchingSubsequence/matchingSubsequences.cpp
@@ -3,16 +3,16 @@ public:
     int numMatchingSubseq(string str, vector<string>& words) {
         int n = words.size();
       map <char, vector <string> > m;
-      for(int i=0;i<words.size();i++){
+      for(int i = 0;i < words.size();i++){
          m[words[i][0]].push_back(words[i]);
       }
       int res = 0;
-      for(int i=0;i<str.size();i++){
+      for(int i = 0;i < str.size();i++){
          char ch = str[i];
          if(m.find(ch) != m.end()){
             vector <string> temp = m[ch];
             m.erase(ch);
-            for(int j=0;j<temp.size();j++){
+            for(int j = 0;j < temp.size();j++){
                if(temp[j].size() == 1){
                   res++;
                } else {
