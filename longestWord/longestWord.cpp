ac7728dc1d98ef906e1cd3f5dd353cab4c7feaf0
Sarita Kumari <ninsaira@gmail.com>
2022-10-20 13:29:59 +0530

    Added white space at appropriate places

--- longestWord/longestWord.cpp
+++ longestWord/longestWord.cpp
@@ -5,21 +5,21 @@ using namespace std;
 int main() {
     
 	string input;
-	cout<<"Enter a string: "<<endl;
+	cout << "Enter a string: " << endl;
 	getline(cin,input);
 	
 	int len = 0;
 	int cur_len = 0;
-	string longest_word="";
-	string cur_word="";
+	string longest_word = "";
+	string cur_word = "";
 	
 	for (int i = 0; i < input.size(); i++) {
 	    
 	    cur_len++;
 	    cur_word += input[i];
 	    
-	    if (input[i]==' ') {
-	        if (len <cur_len) {
+	    if (input[i] == ' ') {
+	        if (len < cur_len) {
 	            len = cur_len;
 	            longest_word = cur_word;
 	        }
@@ -27,6 +27,6 @@ int main() {
 	        cur_word = "";
 	    }
 	}
-	cout<<longest_word<<endl;
+	cout << longest_word << endl;
 	return 0;
 }
