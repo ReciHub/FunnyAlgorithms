6278d1c41f23713f461ce5fd7278ed9efecfd581
Pallaav <pallaav.sethi@gmail.com>
2022-10-24 00:39:08 +0530

    added Credit_card_validator

---
+++ Credit_card_validator.java
@@ -0,0 +1,26 @@
+public class Credit_card_validator {
+    public static void main(String[] args) {
+        String str = "711111111111411";
+        int[] ints = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            ints[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+        for (int i = ints.length - 2; i >= 0; i = i - 2) {
+            int j = ints[i];
+            j = j * 2;
+            if (j > 9) {
+                j = j % 10 + 1;
+            }
+            ints[i] = j;
+        }
+        int sum = 0;
+        for (int i = 0; i < ints.length; i++) {
+            sum += ints[i];
+        }
+        if (sum % 10 == 0) {
+            System.out.println("VALID Credit Card");
+        } else {
+            System.out.println("INVALID Credit Card");
+        }
+    }
+}
