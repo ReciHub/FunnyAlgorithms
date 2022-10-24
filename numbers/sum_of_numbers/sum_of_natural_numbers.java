aa9b87a42f5c70dc4a0902dad546231141cddef9
bmv126 <bmvishwas@gmail.com>
2022-10-24 10:54:18 +0530

    Update sum_of_natural_numbers.java

--- numbers/sum_of_numbers/sum_of_natural_numbers.java
+++ numbers/sum_of_numbers/sum_of_natural_numbers.java
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
 import java.util.Scanner;
 
 class Main{
