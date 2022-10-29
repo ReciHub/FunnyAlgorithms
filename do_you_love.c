25f2b2c8f5cc331a2afb8664b25799974e0165e0
tush47 <110597823+tush47@users.noreply.github.com>
2022-10-29 06:52:19 +0530

    Create do_you_love.c

---
+++ do_you_love.c
@@ -0,0 +1,13 @@
+
+  #include<stdio.h>
+  #include<conio.h>
+
+
+ int main  (void)
+  {
+      int love=3,i;
+ for(i=1;i<100;i++)
+      printf("I %c You ",love);
+ getch();
+       return (0);
+             }
