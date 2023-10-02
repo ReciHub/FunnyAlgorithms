897ad04fd67439cea46d1048d27d6096f0b259f9
vivekumar08 <vivekumar2003bsr@gmail.com>
2023-10-02 19:11:34 +0530

    added comments and description

--- Flame/flame.py
+++ Flame/flame.py
@@ -1,29 +1,46 @@
-import numpy as np
-import itertools as it
+# Importing necessary modules
+import numpy as np  # NumPy library for numerical operations
+import itertools as it  # itertools module for creating iterators for efficient looping
 
-def main(name1,name2):
-    st=[]
+# Main function that calculates FLAME relationship status
+def main(name1, name2):
+    # Empty list to store unique letters from both names
+    unique_letters = []
+
+    # Checking for unique letters in the first name
     for n in name1:
-        if n in name2:
-            pass 
-        else:
-            st.append(n)
-    for n in name2:
-        if n in name1:
-            pass
-        else:
-            st.append(n)
-    game = "FLAME"
-    game_list = ["FRIENDS","LOVE","AFFECTION","MARRIAGE","ENEMY"]
-    l1 = zip(game,game_list)
-    l2 = np.arange(1,len(st)+1)
-    for s,e in zip(l2,it.cycle(l1)):
-        if len(st)==s:
-            print(f"\nyou got the letter {e[0]} from {game}")
-            print("You both are",e[1])
+        # If the letter is not in the second name, add it to the unique letters list
+        if n not in name2:
+            unique_letters.append(n)
 
-            
+    # Checking for unique letters in the second name
+    for n in name2:
+        # If the letter is not in the first name, add it to the unique letters list
+        if n not in name1:
+            unique_letters.append(n)
+
+    # FLAME game letters and corresponding relationship statuses
+    game = "FLAME"
+    game_list = ["FRIEND", "LOVE", "AFFECTION", "MARRIAGE", "ENEMY"]
+
+    # Creating a zipped list of FLAME letters and statuses
+    flame_status = zip(game, game_list)
+
+    # Generating a sequence of numbers from 1 to the number of unique letters
+    unique_letter_count = np.arange(1, len(unique_letters) + 1)
+
+    # Looping through the unique letters and corresponding FLAME statuses
+    for count, (letter, status) in zip(unique_letter_count, it.cycle(flame_status)):
+        # If the count matches the number of unique letters, print the FLAME letter and status
+        if len(unique_letters) == count:
+            print(f"\nYou got the letter '{letter}' from FLAME.")
+            print("Your relationship status is:", status)
+
+# Entry point of the program
 if __name__ == "__main__":
-    name1  = input("Enter Your Name: ")
-    name2 = input("Enter Name of Your partner: ")
-    main(name1,name2)
+    # Getting input from the user for their name and their partner's name
+    name1 = input("Enter Your Name: ")
+    name2 = input("Enter Name of Your Partner: ")
+
+    # Calling the main function with the input names
+    main(name1, name2)
