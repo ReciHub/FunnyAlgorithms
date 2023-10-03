fea2f0e545d82699d415bdc55e81d890ea22bdec
Doodle <piyushrs007@gmail.com>
2023-10-03 20:09:33 +0530

    Update tower_of_hanoi.cpp

--- Tower_of_Hanoi/tower_of_hanoi.cpp
+++ Tower_of_Hanoi/tower_of_hanoi.cpp
@@ -1,34 +1,19 @@
-#include<bits/stdc++.h>
-#define lli long long
+#include <bits/stdc++.h>
 using namespace std;
-lli dp[202];
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+{
+    if (n > 0)
+    {
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+     }
+}
 int main()
 {
-  int t,n;
-   lli x,y;
-   cin >> t;
-   assert(t<=10);
-   while ( t-- ) {
-      cin >> n;
-     assert(n<=200);
-       vector < pair<lli,lli> > v;
-       for ( int i = 0; i < n; i++ ) {
-           cin >> x >> y;
-           assert(x<=1000000000);
-           assert(y<=1000000000);
-           v.push_back(make_pair(x,y));
-       }
-       sort(v.begin(),v.end());
-       dp[0] = v[0].second;
-       lli ans = v[0].second;
-       for ( int i = 1; i < n; i++ ) {
-           dp[i] = v[i].second;
-           for ( int j = 0; j < i; j++ ) {
-               if ( v[i].second > v[j].second && v[i].first > v[j].first ) dp[i] = max(dp[i], dp[j]+v[i].second);
-         }
-  ans = max(ans, dp[i]);
-       }
-       cout << ans << endl;
-   }
-   return 0;
+    int n;
+  cout<<" enter number of disks ";
+  cin>>n;    //user defined number of disks;
+    towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
+    return 0;
 }
