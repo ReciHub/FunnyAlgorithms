e69b917708ad3452c63d83b83ad95089d8f69411
Doodle <piyushrs007@gmail.com>
2023-10-03 20:16:15 +0530

    Create hanoitower.py

---
+++ Hanoi-Tower/hanoitower.py
@@ -0,0 +1,6 @@
+def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
+    if n == 0:
+        return
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
+    print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
