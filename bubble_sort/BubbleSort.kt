b9991e5a2c619e4dd4df73f6f62cae755f1fc753
Moez <65367982+moezmustafa@users.noreply.github.com>
2023-10-11 13:30:17 +0500

    I have created , the genetic string match in cpp

--- bubble_sort/BubbleSort.kt
+++ bubble_sort/BubbleSort.kt
@@ -1,13 +1,36 @@
-fun bubbleSort(numbers: IntArray) {
-    for (pass in 0 until (numbers.size - 1)) {
-        // A single pass of bubble sort
-        for (currentPosition in 0 until (numbers.size - 1)) {
-            // This is a single step
-            if (numbers[currentPosition] > numbers[currentPosition + 1]) {
-                val tmp = numbers[currentPosition]
-                numbers[currentPosition] = numbers[currentPosition + 1]
-                numbers[currentPosition + 1] = tmp
-            }
+
+import java.util.*
+import java.lang.*
+import java.io.*
+
+class BubbleSort {
+    internal fun bubbleSort(arr: IntArray) {
+        val n = arr.size
+        for (i in 0 until n - 1)
+            for (j in 0 until n - i - 1)
+                if (arr[j] > arr[j + 1]) {
+                    val temp = arr[j]
+                    arr[j] = arr[j + 1]
+                    arr[j + 1] = temp
+                }
+    }
+
+    internal fun printArray(arr: IntArray) {
+        val n = arr.size
+        for (i in 0 until n)
+            print(arr[i].toString() + " ")
+        println()
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val ob = BubbleSort()
+            val arr = intArrayOf(120, 24, 29, 172, 2, 1, 5)
+            ob.bubbleSort(arr)
+            println("Sorted array")
+            ob.printArray(arr)
         }
     }
 }
