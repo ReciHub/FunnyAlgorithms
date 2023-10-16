64a9af9793253091a91085e8bc7e8ea5fa50fda6
theinit01 <tushar3145@gmail.com>
2023-10-16 12:02:26 -0400

    Added heapsort.c

---
+++ Heap Sort/heapsort.py
@@ -0,0 +1,36 @@
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[l] > arr[largest]:
+        largest = l
+
+    if r < n and arr[r] > arr[largest]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+
+def printArray(arr):
+    for element in arr:
+        print(element, end=" ")
+    print()
+
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6, 7]
+    heapSort(arr)
+    print("Sorted array is")
+    printArray(arr)
+
