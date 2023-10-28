4cd7f01681c8c6be9219cf506f0e68e6c681385b
Filipe Moreno <79486720+MoonHawlk@users.noreply.github.com>
2023-10-27 22:52:11 -0300

    Create beecrowd_1000.py

---
+++ Beecrowd/beecrowd_1000.py
@@ -0,0 +1,9 @@
+Bem-vindo ao beecrowd!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
