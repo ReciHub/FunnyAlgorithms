4a4ba3e2b8f6d6ea7f0d91c29935edea935e154a
moosemorse <a.ivanov6174@gmail.com>
2025-12-19 21:35:59 +0000

    fix+refactor: Rock paper scissors resolves type warnings and cleanup logic.

--- Rock paper scissors/C++/Rock-Paper-Scissors-Game.cpp
+++ Rock paper scissors/C++/Rock-Paper-Scissors-Game.cpp
@@ -1,105 +1,107 @@
-#include <iostream>
 #include <algorithm>
-#include <vector>
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <optional>
+#include <string_view>
+#include <tuple>
 
-// Function to compare the choices [0 = tie, -1 = computer win, 1 = user win, -10 = invalid user input]
-int compare(std::string &userChoice, std::string& computerChoice) 
-{
-    if (userChoice == computerChoice) 
-    {
-        return 0;
-    }
-    else if (userChoice == "rock") 
-    {
-        if (computerChoice == "paper") 
-        {
-            return -1;
-        } 
-        else 
-        {
-            return 1;
-        }
-    }
-    else if (userChoice == "paper") 
-    {
-        if (computerChoice == "scissors") 
-        {
-            return -1;
-        } 
-        else 
-        {
-            return 1;
-        }
-    }
-    else if (userChoice == "scissors") 
-    {
-        if (computerChoice == "rock") 
-        {
-            return -1;
-        } 
-        else 
-        {
-            return 1;
-        }
-    }
-    else 
-    {
-        return -10;
-    }
+enum class GameStatus {
+  UserWins,
+  AIWins,
+  Tie,
+};
+
+enum class Move { Rock, Paper, Scissors };
+
+// Function to find who wins between user choice and computer choice
+GameStatus compare(Move userChoice, Move computerChoice) {
+  // 1. If both choices are same, it's a tie
+  if (userChoice == computerChoice) return GameStatus::Tie;
+
+  using WinningCombinationsArr = std::array<std::tuple<Move, Move>, 3>;
+  constexpr WinningCombinationsArr winning_combinations{
+      {{Move::Rock, Move::Scissors},
+       {Move::Paper, Move::Rock},
+       {Move::Scissors, Move::Paper}}};
+
+  // 2. If given combination found in winning_combinations, user wins
+  for (auto [winner, loser] : winning_combinations) {
+    if (userChoice == winner && computerChoice == loser)
+      return GameStatus::UserWins;
+  }
+
+  // 3. Otherwise, lose
+  return GameStatus::AIWins;
 }
 
-int main()
-{
-    int userScore = 0, computerScore = 0;
-    std::vector<std::string> choice = {"rock", "paper", "scissors"};
+int main() {
+  int userScore = 0, computerScore = 0;
+  constexpr std::array<Move, 3> choice = {Move::Rock, Move::Paper,
+                                          Move::Scissors};
 
-    // Seed for generating different random numbers every time
-    srand(time(0));
-    
-    while (true) 
-    {
-        // Taking user's choice
-        std::cout << "Choose one from rock, paper or scissors  [press q to quit] ";
-        std::string userInput;
-        std::cin >> userInput;
-        std::cout << "\n\n\n";
-        
-        // Convert entire string to lowercase
-        std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::tolower);
+  // Seed for generating different random numbers every time
+  srand(static_cast<unsigned>(time(0)));
 
-        // Check for quit
-        if (userInput == "q") 
-        {
-            break;
-        }
+  std::string userInput;
+  while (true) {
+    // Taking user's choice
+    std::cout << "Choose one from rock, paper or scissors  [press q to quit] ";
+    std::cin >> userInput;
+    std::cout << "\n\n\n";
 
-        // Generate computer's choice randomly
-        std::string computerChoice = choice[rand() % 3];
-        
-        int result = compare(userInput, computerChoice);
+    // Convert entire string to lowercase
+    std::transform(userInput.begin(), userInput.end(), userInput.begin(),
+                   ::tolower);
 
-        switch (result)
-        {
-            case 0:
-            {
-                std::cout << "It's a tie !" << std::endl;
-            } break;
-            case 1:
-            {
-                std::cout << "User wins !!" << std::endl;
-                userScore++;
-            } break;
-            case -1:
-            {
-                std::cout << "Computer wins !!" << std::endl;
-                computerScore++;
-            } break;
-            case -10:
-            {
-                std::cout << "Invalid input. Please enter one of rock, paper or scissors" << std::endl;
-            } break;
-        }
-        std::cout << "\n\n=================\nCurrent scores :\nUser : " << userScore << "\nComputer : " << computerScore << "\n=================\n\n";
+    // Check for quit
+    if (userInput == "q") {
+      break;
     }
-    std::cout << "\n\n=================\nFinal scores :\nUser : " << userScore << "\nComputer : " << computerScore << "\n=================\n\n"; 
+
+    // parse user input into enum class Move,
+    // if fails then optional doesn't contain value
+    auto parse_move = [](std::string_view s) -> std::optional<Move> {
+      if (s == "rock") return Move::Rock;
+      if (s == "paper") return Move::Paper;
+      if (s == "scissors") return Move::Scissors;
+      return std::nullopt;
+    };
+
+    std::optional<Move> user_move{parse_move(userInput)};
+
+    if (!user_move) {
+      std::cout << "Invalid input. Please enter one of rock, paper or scissors"
+                << std::endl;
+      continue;
+    }
+
+    // Generate computer's choice randomly
+    Move computer_move{choice[static_cast<unsigned>(rand()) % 3]};
+
+    assert(user_move.has_value() &&
+           "parse_move should've caught invalid user move.");
+
+    // Find result from user move and computer move
+    GameStatus result = compare(user_move.value(), computer_move);
+
+    switch (result) {
+      case GameStatus::Tie: {
+        std::cout << "It's a tie !" << std::endl;
+      } break;
+      case GameStatus::UserWins: {
+        std::cout << "User wins !!" << std::endl;
+        userScore++;
+      } break;
+      case GameStatus::AIWins: {
+        std::cout << "Computer wins !!" << std::endl;
+        computerScore++;
+      } break;
+    }
+    std::cout << "\n\n=================\nCurrent scores :\nUser : " << userScore
+              << "\nComputer : " << computerScore << "\n=================\n\n";
+  }
+  std::cout << "\n\n=================\nFinal scores :\nUser : " << userScore
+            << "\nComputer : " << computerScore << "\n=================\n\n";
+  return 0;
 }
